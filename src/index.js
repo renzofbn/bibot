7e4822cd1697be3a7b860648a71a127c2534c49e
renzofbn <renzofbn@tutanota.com>
2023-09-20 12:16:16 -0500

    change logs

--- src/index.js
+++ src/index.js
@@ -4,23 +4,18 @@ import { WhatsappSessionManager } from './msg_controllers/client_controller.js';
 import winston from 'winston';
 
 export const wtsSessionManager = new WhatsappSessionManager();
+
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.cli(),
   defaultMeta: { service: 'user-service' },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-    // Transporte para imprimir registros en la consola
+    new winston.transports.File({ filename: 'logs/errors.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/server.log' }),
     new winston.transports.Console()
   ],
 });
 
 app.listen(PORT)
-
 logger.info('✅ Server running at ' + new Date().toLocaleString());
 logger.info(`Server running on port ${PORT}`);
